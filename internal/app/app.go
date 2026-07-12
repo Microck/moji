5e5d5c55c80c82c8a1e4cb710f5ec2f833b94a23
Microck <contact@micr.dev>
2026-07-12 12:26:48 +0000

    chore(release): prepare v0.3.1

--- internal/app/app.go
+++ internal/app/app.go
@@ -16,7 +16,7 @@ import (
 	"golang.org/x/term"
 )
 
-var Version = "0.3.0"
+var Version = "0.3.1"
 var ReleaseMarker = "moji-release-version:development:moji-marker-end"
 
 // allowPrivateBuild is set only on the subprocess binary built by the E2E
